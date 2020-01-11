03996f26097e4bd4e4a1fc2d64138b48275839f8
Kyle Umstatter <blackstar257@gmail.com>
2020-01-10 20:21:47 -0800

    cleanup some more relativityg

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+---
+---
+
 SimpleJekyllSearch({
   searchInput: document.getElementById("jets-search"),
   resultsContainer: document.getElementById("results-container"),
   searchResultTemplate:
     '<div class="item"><i class="blue map marker icon"></i><div class="content"><div class="header"><a class="header" href="{url}">{name}</a></div>{comment}</div></div>',
-  json: "api/v1/all.json",
+  json: "{{ 'api/v1/all.json' | relative_url }}",
   fuzzy: false,
   templateMiddleware: function(prop, value, template) {
     if (prop === "comment" && value == "") {
